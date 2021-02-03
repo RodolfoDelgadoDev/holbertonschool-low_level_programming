3f49b3a1c86a16b8d385cadd44edfe7a2513560d
root <rodrigo.delgado3132@gmail.com>
2021-02-03 19:09:17 +0000

    Fix 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -6,8 +6,6 @@
  */
 int main(void)
 {
-  char str1[15];
-  strcpy(str1, "\"Programming is like building a multilingual puzzle");
-  puts(str1);
-  return (0);
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
 }
