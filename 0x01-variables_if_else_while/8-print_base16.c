1f84dd9f7cd008d7b50fbdf94bd47a7beaec3b24
root <rodrigo.delgado3132@gmail.com>
2021-02-04 19:21:53 +0000

    Added 8

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+char ch;
+int nm;
+for (nm = '0'; nm <= '9'; nm++)
+{
+putchar(nm);
+}
+for (ch = 'a'; ch <= 'f'; ch++)
+{
+putchar(ch);
+}
+return (0);
+}
