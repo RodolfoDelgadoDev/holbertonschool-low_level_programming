2ce7465a8a17b7d1bba090a1888845f719a85587
root <rodrigo.delgado3132@gmail.com>
2021-02-04 18:58:33 +0000

    Added 6

---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+int nm;
+for (nm = '0' ; nm <= '9' ; nm++)
+{
+putchar(nm);
+}
+putchar('\n');
+return (0);
+}
