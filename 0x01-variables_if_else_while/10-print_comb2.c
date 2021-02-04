d42fedd6052bf48acdddb723bd8273ed30409aad
root <rodrigo.delgado3132@gmail.com>
2021-02-04 21:34:01 +0000

    fix 10

--- 0x01-variables_if_else_while/10-print_comb2.c
+++ 0x01-variables_if_else_while/10-print_comb2.c
@@ -6,10 +6,11 @@
  */
 int main(void)
 {
-int nm , num;
+int nm;
+int num;
 for (nm = 48 ; nm <= 57 ; nm++)
 {
-for (num = 48 ; num <= 57 ; num++) 
+for (num = 48 ; num <= 57 ; num++)
 {
 putchar(nm);
 putchar(num);
@@ -17,6 +18,8 @@ if (nm != 9 && num != 9)
 {
 putchar(',');
 putchar(' ');
+}
+}
 }
 putchar('\n');
 return (0);
