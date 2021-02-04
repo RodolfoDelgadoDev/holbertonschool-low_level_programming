c320983c9dfb110b8445b547d87ea15afcfd40af
root <rodrigo.delgado3132@gmail.com>
2021-02-04 18:16:02 +0000

    Fix 4

--- 0x01-variables_if_else_while/4-print_alphabt.c
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 #include <stdlib.h>
-#include <time.h
+#include <time.h>
 /**
  * main - Entry point
  *
@@ -9,11 +9,13 @@
 int main(void)
 {
 char ch;
+
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
-putchar(ch);
 if (ch == 'e' || ch == 'q')
 ch++;
+putchar(ch);
 }
+putchar('\n');
 return (0);
 }
