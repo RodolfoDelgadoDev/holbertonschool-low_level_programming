cb28b5941331cfc13b569b1f36ae5386531d42a4
root <rodrigo.delgado3132@gmail.com>
2021-02-09 13:39:19 +0000

    Fix 1

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,12 +1,13 @@
 #include "holberton.h"
 /**
- * main - Entry point
+ * print_alphabet - Entry point
  *
- * Return: Always 0 (Success)
+ * Return: gil (Success)
  */
-void print_alphabet(void);
+void print_alphabet(void)
 {
 char ch;
+
 for (ch = 'a' ; ch <= 'z' ; ch++)
 {
 _putchar(ch);
