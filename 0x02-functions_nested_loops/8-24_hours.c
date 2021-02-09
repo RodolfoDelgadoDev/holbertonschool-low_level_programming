e43450629a5a8f09323d7a98706c009630e55df6
root <rodrigo.delgado3132@gmail.com>
2021-02-09 22:20:43 +0000

    Fix 8

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -1,4 +1,5 @@
 #include "holberton.h"
+#include<stdbool.h>
 /**
  * jack_bauer - Entry point
  *
@@ -11,15 +12,14 @@ int n0;
 int n1;
 int n2;
 int n3;
-
+bool b = true; 
 for (n0 = '0'; n0 <= '2'; n0++)
 {
-for (n1 = '0'; n1 <= '3'; n1++)
+for (n1 = '0'; n1 <= '9'; n1++ && b == true)
 {
 for (n2 = '0'; n2 <= '5'; n2++)
 {
 for (n3 = '0'; n3 <= '9'; n3++)
-{
 {
 _putchar(n0);
 _putchar(n1);
@@ -29,9 +29,10 @@ _putchar(n3);
 _putchar('\n');
 }
 }
+if ( n0 == '2' && n1 == '3')   
+{
+b = false;
 }
 }
 }
 }
-
-
