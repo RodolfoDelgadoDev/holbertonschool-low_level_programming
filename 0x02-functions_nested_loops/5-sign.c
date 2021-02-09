c86c77fb96e4141e341f998ef3ed6f05426cfa8e
root <rodrigo.delgado3132@gmail.com>
2021-02-09 17:02:20 +0000

    Added 5

---
+++ 0x02-functions_nested_loops/5-sign.c
@@ -0,0 +1,24 @@
+#include "holberton.h"
+/**
+ * print_sign - Entry point
+ * @n: nothing
+ * Return: n (Success)
+ */
+int print_sign(int n)
+{
+if (n > 1)
+{
+_putchar('+');
+return (1);
+}
+else if (n == 0)
+{
+_putchar('0');
+return (0);
+}
+else
+{
+_putchar('-');
+return (-1);
+}
+}
