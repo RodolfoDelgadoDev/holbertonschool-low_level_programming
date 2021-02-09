0505b5f169829741d8706995438f68ba8e02f1a3
root <rodrigo.delgado3132@gmail.com>
2021-02-09 12:45:45 +0000

    Added holberton.h,1-alphabet.c and fix 0.holberton.c

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -1,5 +1,4 @@
-#include <stdio.h>
-
+#include "holberton.h"
 /**
  * main - Entry point
  *
@@ -7,6 +6,28 @@
  */
 int main(void)
 {
-printf("HolbertonÂ\n");
+char H = 'H';
+char o = 'o';
+char l = 'l';
+char b = 'b';
+char e = 'e';
+char r = 'r';
+char t = 't';
+char o = 'o';
+char n = 'n';
+int s;
+for (s = 1 ; s <= 9 ; c++)
+{
+_putchar(H);
+_putchar(o);
+_putchar(l);
+_putchar(b);
+_putchar(e);
+_putchar(r);
+_putchar(t);
+_putchar(o);
+_putchar(n);
+}
+_putchar('\n');
 return (0);
 }
