f9d6f2a7e31dd2ab14f7058f4984363793db2098
root <rodrigo.delgado3132@gmail.com>
2021-02-09 13:48:18 +0000

    added 2-main.c and 2-print_al.

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+/**
+ * print_alphabet_x10 - Entry point
+ *
+ * Return: gil (Success)
+ */
+void print_alphabet_x10(void)
+{
+char ch;
+int cn;
+while (cn <= 10)
+{
+for (ch = 'a' ; ch <= 'z' ; ch++)
+{
+_putchar(ch);
+}
+_putchar('\n');
+cn++;
+}
+}
