89dd7ec84fda4899491a974076151c5ad67e094e
root <rodrigo.delgado3132@gmail.com>
2021-02-09 12:49:26 +0000

    fix 0

--- 0x02-functions_nested_loops/0-holberton.c
+++ 0x02-functions_nested_loops/0-holberton.c
@@ -6,27 +6,18 @@
  */
 int main(void)
 {
-char H = 'H';
-char o = 'o';
-char l = 'l';
-char b = 'b';
-char e = 'e';
-char r = 'r';
-char t = 't';
-char o = 'o';
-char n = 'n';
 int s;
-for (s = 1 ; s <= 9 ; c++)
+for (s = 1 ; s <= 1 ; c++)
 {
-_putchar(H);
-_putchar(o);
-_putchar(l);
-_putchar(b);
-_putchar(e);
-_putchar(r);
-_putchar(t);
-_putchar(o);
-_putchar(n);
+_putchar('H');
+_putchar('o');
+_putchar('l');
+_putchar('b');
+_putchar('e');
+_putchar('r');
+_putchar('t');
+_putchar('o');
+_putchar('n');
 }
 _putchar('\n');
 return (0);
