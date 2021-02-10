345ffd59f41e9c512a9700f79235ddb7cb64cb3e
root <rodrigo.delgado3132@gmail.com>
2021-02-10 16:56:59 +0000

    Fix 10

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,10 +1,12 @@
 #include "holberton.h"
 /**
  * add - Entry point
- *
+ * @x: aa
+ * @y: aaaa
  * Return: Always 0 (Success)
  *
  */
-int add(int x, int y){
-  return (x + y);
+int add(int x, int y)
+{
+return (x + y);
 }
