cb4442d1518739903fc31972ef4cbdfa55b1ce92
root <rodrigo.delgado3132@gmail.com>
2021-02-11 14:29:01 +0000

    Added 2,3,4,5

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,13 @@
+#include "holberton.h"
+#include <stdio.h>
+/**
+ * mul - check the code for Holberton School students.
+ * @a: a
+ * @b: b
+ * Return: Always 0.
+ */
+int mul(int a, int b)
+{
+int i = a * b;
+return (i);
+}
---
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+
+/**
+ * main - check the code for Holberton School students.
+ *
+ * Return: Always 0.
+ */
+void more_numbers(void)
+{
+int i = 0;
+int n1;
+int n2;
+
+while (i <= 0)
+{
+for (n1 = 0; n1 <= 1;)
+{
