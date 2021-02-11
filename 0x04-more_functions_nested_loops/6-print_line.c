e74b4c2dc1ea4c49328b19863770414cd2b240e8
root <rodrigo.delgado3132@gmail.com>
2021-02-11 19:11:06 +0000

    Add 6

---
+++ 0x04-more_functions_nested_loops/6-print_line.c
@@ -0,0 +1,14 @@
+#include "holberton.h"
+
+/**
+ * print_line - check the code for Holberton School students.
+ * @n: n
+ * Return: Always 0.
+ */
+void print_line(int n)
+{
+while ('0' < n)
+{
+_putchar('_');
+}
+}
