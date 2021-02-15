95ad5a355847950b093c8765ab5f1fc637b79073
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-15 18:58:52 +0000

    Added 2

---
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -0,0 +1,19 @@
+#include "holberton.h"
+/**
+ * _strlen - check the code for Holberton School students.
+ * @s: sabaduki
+ * Return: Always 0.
+ */
+
+
+int _strlen(char *s)
+{
+int a = 0;
+int c = 0;
+while (s[a] != '\0')
+{
+a++;
+c++;
+}
+return (c);
+}
