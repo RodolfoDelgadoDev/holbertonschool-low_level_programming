58454f2a73fa62bdaedf96659875f1cba3d74f8d
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-15 19:14:52 +0000

    Added ex. 3

---
+++ 0x05-pointers_arrays_strings/3-puts.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+/**
+ * _strlen - check the code for Holberton School students.
+ * @str: string
+ * Return: Always 0.
+ */
+
+void _puts(char *str)
+{
+int a = 0;
+while (str[a] != '\0')
+{
+_putchar(str[a]);
+a++;
+}
+_putchar('\n');
+}
