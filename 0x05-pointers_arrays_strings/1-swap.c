3b8cd4ef931eff3c9025c0de85da04497d61431e
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-15 18:24:28 +0000

    Added 1 and prototypes for 1

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+
+/**
+ * swap_int - check the code for Holberton School students.
+ * @a:ba
+ * @b: b
+ * Return: Always 0.
+ */
+
+void swap_int(int *a, int *b)
+{
+int r;
+r = *a;
+*a = *b;
+*b = r;
+}
