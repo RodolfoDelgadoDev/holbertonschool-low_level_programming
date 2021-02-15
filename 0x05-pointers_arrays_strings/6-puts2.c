4c89cf085907758d1d208600bb745b785fd6d35b
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-15 22:39:26 +0000

    Fix 6

--- 0x05-pointers_arrays_strings/6-puts2.c
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -8,7 +8,6 @@
 void puts2(char *str)
 {
 int a = 0;
-
 while (str[a] != '\0')
 {
 if (a == 0)
@@ -17,6 +16,8 @@ _putchar(str[0]);
 a++;
 }
 a++;
+if (str[a] == '\0')
+break;
 _putchar(str[a]);
 a++;
 }
