52bcb026601ea166c2ec3c5804e8e1ee68e336a1
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-16 22:22:07 +0000

    Added 9

---
+++ 0x05-pointers_arrays_strings/9-strcpy.c
@@ -0,0 +1,21 @@
+#include "holberton.h"
+#include "2-strlen.c"
+/**
+ * _strcpy - check the code for Holberton School students.
+ * @dest: dest
+ * @src: src
+ * Return: Always 0.
+ */
+char *_strcpy(char *dest, char *src)
+{
+int i = 0;
+
+int a = _strlen(src);
+a++;
+while (i <= a)
+{
+dest[i] = src[i];
+i++;
+}
+return (dest);
+}
