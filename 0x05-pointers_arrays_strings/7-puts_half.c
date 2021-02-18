1fb99407eb2bddc428948f6f858db952807269df
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-18 00:00:07 +0000

    Added 7

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -1,12 +1,21 @@
 #include "holberton.h"
 #include "2-strlen.c"
 /**
- * main - check the code for Holberton School students.
- *
+ * puts_half - check the code for Holberton School students.
+ * @str: Str
  * Return: Always 0.
  */
 void puts_half(char *str)
 {
-  int tm;
-  int mitad;
-int largo = 
+int tm;
+int mitad;
+int largo = _strlen(str);
+if ((_strlen(str) % 2) == 0)
+mitad = largo / 2;
+else
+mitad = (largo - 1) / 2;
+tm = largo - mitad;
+for ( ; tm < largo; tm++)
+_putchar(str[tm]);
+_putchar(10);
+}
