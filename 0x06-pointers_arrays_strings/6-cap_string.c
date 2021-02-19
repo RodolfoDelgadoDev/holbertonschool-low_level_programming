c02f914301b06163899fd32fd3be85dd1ad43af0
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-19 23:37:55 +0000

    fix

--- 0x06-pointers_arrays_strings/6-cap_string.c
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -10,10 +10,10 @@ char *cap_string(char *s)
 int i;
 for (i = 0; s[i] != '\0'; i++)
 {
-if ( s[0] >= 'a' && s[0] <= 'z')
+if (s[0] >= 'a' && s[0] <= 'z')
 s[0] = s[0] - 32;
-if ( (s[i - 1] == ' ') || (s[i - 1] == '\t') )
-   s[i] = s[i] - 32;
+if ((s[i - 1] == ' ') || (s[i - 1] == '\t'))
+s[i] = s[i] - 32;
 }
 return (s);
 }
