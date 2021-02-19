e1c4f416c2a5e0419c03d6e08ecbb567a6e6ca43
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-19 21:02:14 +0000

    Add 3

---
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+/**
+ * _strncat - acheck the code for Holberton School students.
+ * @s1: s1
+ * @s2: s2 
+ * Return: Always 0.
+ */
+int _strcmp(char *s1, char *s2)
+{
+int a = 0;
+int b = 0;
+int c = 0;
+int d = 0;
+while (s1[a] != '\0')
+a++;
+while (s2[b] != '\0')
+b++;
+if (a != b)
+{
+return ('-15');
+}
+ for ( ; c <= a; c++, d++)
+{
+  if (s1[c] == s2[d])
+    {
+ 
