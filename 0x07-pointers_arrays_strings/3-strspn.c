d749dcb4d1b730bcc4902fcec05ae54cbe0dee4c
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-23 20:13:14 +0000

    Added 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "holberton.h"
+#define NULL ((void *)0)
+/**
+ * _strspn - pn
+ * @c: the address of memory to print
+ * @s: the size of the memory to print
+ *
+ * Return: c
+ */
+unsigned int _strspn(char *s, char *accept)
+{
+
+	while (*s != '\0')
+	{
+		if (*s == c)
+		{
+			return (s);
+
+		}
+
+		s++;
+	}
+	if (*s == c)
+		return (s);
+
+	return (NULL);
+
+}
