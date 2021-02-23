d2992448642985b82525053ed50a56e1f3791d1a
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-23 17:08:05 +0000

    Added 0x07, and 0,holberton.h,_putchar.c and deleted 5 from 0x05

---
+++ 0x07-pointers_arrays_strings/0-memset.c
@@ -0,0 +1,16 @@
+#include "holberton.h"
+/**
+ * *_memset - memexd
+ * @s: the address of memory to print
+ * @b: the size of the memory to print
+ * @n: nnnnnnnnnnnnnnnnnnnnnnnnnn
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+unsigned int i = 0;
+int a = 0;
+for ( ; i < n ; i++, a++)
+	s[a] = b;
+return (s);
+}
