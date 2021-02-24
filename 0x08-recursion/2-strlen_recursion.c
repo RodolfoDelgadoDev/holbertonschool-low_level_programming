85d7e0faa024583663117d20056186b5b226d7fa
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-24 21:57:56 +0000

    fix 2

--- 0x08-recursion/2-strlen_recursion.c
+++ 0x08-recursion/2-strlen_recursion.c
@@ -1,25 +1,34 @@
 #include "holberton.h"
 
 /**
- * _strlen_recursion - check the code for Holberton School students.
+ * contador - aaaaaa
  * @s: ssss
+ * @c: cc
  * Return: Always 0.
  */
-int contador(char *s ,int c)
+int contador(char *s, int c)
 {
 	if (*s != '\0')
 	{
-		return (contador (s + 1, c + 1));
+		return (contador(s + 1, c + 1));
 	}
 	return (c);
 
 }
+
+/**
+ * _strlen_recursion - check the code for Holberton School students.
+ * @s: ssss
+ * Return: Always 0.
+ */
+
 
 
 int _strlen_recursion(char *s)
 {
 
 	int c = 0;
+
 	if (*s == '\0')
 		return (0);
 	return (contador(s, c));
