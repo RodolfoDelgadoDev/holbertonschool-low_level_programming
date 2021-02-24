4f42613857312f97ad5ba9f4926f3b2074a4d917
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-24 22:10:47 +0000

    fix 3

--- 0x08-recursion/3-factorial.c
+++ 0x08-recursion/3-factorial.c
@@ -1,15 +1,15 @@
 #include "holberton.h"
 
 /**
- * main - check the code for Holberton School students.
- *
+ * factorial - check the code for Holberton School students.
+ * @n: n
  * Return: Always 0.
  */
 int factorial(int n)
 {
 	if (n <= 0)
-		return(-1);
+		return (-1);
 	if (n == 1)
-		return(1);
-	return n * factorial(n - 1);
+		return (1);
+	return (n * factorial(n - 1));
 }
