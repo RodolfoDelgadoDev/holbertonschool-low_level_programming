fedb4f7b33daf6db7740e94eff64940b2b828c37
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-26 21:03:21 +0000

    Added 9 and 0

---
+++ 0x09-static_libraries/0-strcat.c
@@ -0,0 +1,12 @@
+#include "holberton.h"
+/**
+ * char *_strcat - check the code for Holberton School students.
+ * @dest: dest
+ * @src: src
+ * Return: Always 0.
+ */
+char *_strcat(char *dest, char *src)
+{
+	int a = 0;
+	int b = 0;
+}
