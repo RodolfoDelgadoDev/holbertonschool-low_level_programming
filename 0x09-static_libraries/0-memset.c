be17086e42f15762b1efb3d5095a9dd528f8a552
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-27 19:39:44 +0000

    Fix 0

--- 0x09-static_libraries/0-memset.c
+++ 0x09-static_libraries/0-memset.c
@@ -1,1 +1,17 @@
-char *_memset(char *s, char b, unsigned int n);
+#include "holberton.h"
+/**
+ * *_memset - memexd
+ * @s: the address of memory to print
+ * @b: the size of the memory to print
+ * @n: nnnnnnnnnnnnnnnnnnnnnnnnnn
+ * Return: Nothing.
+ */
+char *_memset(char *s, char b, unsigned int n)
+{
+	unsigned int i = 0;
+	int a = 0;
+	char *c = s;
+	for ( ; i < n ; i++, a++)
+		s[a] = b;
+	return (c);
+}
