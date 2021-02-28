330eb57e720657eb6daad8789f77e50a6cf278dc
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-28 20:06:30 +0000

    Added 4 and ex.

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument counter
+ * @argv: argument v.
+ * Return: Always 0.
+ */
+
+int main(int argc, char *argv[])
+{
+	int c;
+	int cuenta = 0;
+	if (argc == 1)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	for (c = 1; c < argc; c++)
+	{
+		if (*argv[c] >= 48 && *argv[c] <= 57)
+			cuenta += atoi(argv[c]);
+		else
+		{
+			printf("Error\n");
+			return (1);
+		}
+	}
+
+	printf("%d\n", cuenta);
+	return (0);
+
+}
