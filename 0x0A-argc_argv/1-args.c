e9ee1b38d649fd56124eebb9eb540aa785ae35fe
Ro8s <rodrigo.delgado3132@gmail.com>
2021-02-28 16:07:28 +0000

    added ex 1, nargs and a.out for texting

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,17 @@
+#include "holberton.h"
+#include <stdio.h>
+
+/**
+ * main - check the code for Holberton School students.
+ * @argc: argument counter
+ * @argv: argument v.
+ * Return: Always 0.
+ */
+int main(int argc, char *argv[])
+{
+	argv = argv;
+	printf("%d\n", argc - 1);
+	return (0);
+
+
+}
