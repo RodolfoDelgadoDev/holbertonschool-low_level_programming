3b1801ddf37ff60e07cd63c91606944f8ef56063
Ro8s <rodrigo.delgado3132@gmail.com>
2021-03-02 17:39:43 +0000

    Added proyect 0x0B, with ex 0, README, and libs

---
+++ 0x0B-malloc_free/0-create_array.c
@@ -0,0 +1,26 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * create_array - prints buffer in hexa
+ * @c: the address of memory to print
+ * @size: the size of the memory to print
+ *
+ * Return: Nothing.
+ */
+
+char *create_array(unsigned int size, char c)
+{
+	unsigned int a;
+	char *cha = malloc(sizeof(char) * size);
+
+	if (cha == NULL)
+	{
+		return (NULL);
+	}
+
+	for (a = 0; a <= size; a++)
+		cha[a] = c;
+	return (cha);
+}
