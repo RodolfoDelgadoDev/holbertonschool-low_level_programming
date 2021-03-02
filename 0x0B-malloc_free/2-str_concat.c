dcb98940d96e07b1843c68a80b096a93478bf149
Ro8s <rodrigo.delgado3132@gmail.com>
2021-03-02 22:46:24 +0000

    Fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,19 +12,24 @@ char *str_concat(char *s1, char *s2)
 	int a = 0;
 	int b = 0;
 	char *conc;
+	int c;
 	int d;
 	int j;
 
+	if (s1 == NULL || s2 == NULL)
+		return (NULL);
 	while (s1[a] != '\0')
 		a++;
 	while (s2[b] != '\0')
 		b++;
-	conc = malloc(sizeof(char) + (a + b) + 1);
+	b++;
+	c = a + b;
+	conc = malloc(sizeof(char) * c);
 	if (conc == NULL)
 		return (NULL);
 	if (s1 == '\0' && s2 == '\0')
 	{
-		conc[0] = '\0';
+		conc = s2;
 		return (conc);
 	}
 	if (s1 == '\0')
@@ -41,6 +46,5 @@ char *str_concat(char *s1, char *s2)
 		conc[d] = s1[d];
 	for (j = 0; j < b; j++)
 		conc[a + j] = s2[j];
-	conc[j] = '\0';
 	return (conc);
 }
