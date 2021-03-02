8097a953ef51feaeaea8866124e119a140bf5feb
Ro8s <rodrigo.delgado3132@gmail.com>
2021-03-02 21:43:32 +0000

    Fix

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -3,45 +3,29 @@
 #include <stdlib.h>
 
 /**
- * str_concat - prints buffer in hexa
- * @s1: the size of the memory to print
- * @s2: s2
+ * _strdup - prints buffer in hexa
+ * @str: the size of the memory to print
  * Return: Nothing.
  */
 
-char *str_concat(char *s1, char *s2)
+char *_strdup(char *str)
 {
 	int a = 0;
 	int b = 0;
-	char *conc;
-	int c;
-	int d;
-	int j;
+	char *new;
 
-	if (s1 == NULL || s2 == NULL)
+	if (str == NULL)
 		return (NULL);
-	while (s1[a] != '\0')
+	while (str[a] != '\0')
 		a++;
-	while (s2[b] != '\0')
-		b++;
-	b++;
-	c = a + b;
-	conc = malloc(sizeof(char) * c);
-	if (conc == NULL)
+	a++;
+	new = malloc(sizeof(char) * a);
+	if (new == NULL)
 		return (NULL);
-	if (s1 == '\0')
-	{
-		conc = s2;
-		return (conc);
-	}
-	if (s2 == '\0')
-	{
-		conc = s1;
-		return (conc);
-	}
-	for (d = 0; d < a; d++)
-		conc[d] = s1[d];
-	for (j = 0; j < b; j++)
-		conc[a + j] = s2[j];
-	return (conc);
+	for (; b <= a; b++)
+		new[b] = str[b];
+
+	return (new);
+
+
 }
