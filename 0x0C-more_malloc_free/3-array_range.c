37127415b2ac014cad3f6edd0d3514c0fbfa4282
Ro8s <rodrigo.delgado3132@gmail.com>
2021-03-04 21:56:23 +0000

    Add 3

---
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+
+/**
+ * array_range - prints buffer in hexa
+ * @min: min
+ * @max: max
+ *
+ * Return: Nothing.
+ */
+
+int *array_range(int min, int max)
+{
+	if (min > max)
+		return (NULL);
+
+
+}
