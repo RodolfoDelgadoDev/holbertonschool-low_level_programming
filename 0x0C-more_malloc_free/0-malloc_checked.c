609f39d7d70aabf725cd0e25a34ad66cc3afe668
Ro8s <rodrigo.delgado3132@gmail.com>
2021-03-04 14:30:26 +0000

    Added 0x=C, ex 0 and his main

---
+++ 0x0C-more_malloc_free/0-malloc_checked.c
@@ -0,0 +1,20 @@
+#include "holberton.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <limits.h>
+
+/**
+ * malloc_checked - prints buffer in hexa
+ * @b: the size of the memory to print
+ * Return: Nothing.
+ */
+void *malloc_checked(unsigned int b)
+{
+	void *p;
+
+	p = malloc(b);
+	if (p == NULL)
+		return ("98");
+	return (p);
+
+}
