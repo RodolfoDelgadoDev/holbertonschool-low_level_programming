260427009bf4926eb01b9cfd925bb3ea261de686
Ro8s <rodrigo.delgado3132@gmail.com>
2021-03-04 22:10:24 +0000

    added 3

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,8 +13,17 @@
 
 int *array_range(int min, int max)
 {
+	int *a;
+	int c;
+
 	if (min > max)
 		return (NULL);
 
+	a = malloc(sizeof(int) * (max - min) + 1);
+	if (a == NULL)
+		return (NULL);
+	for (c = 0; (c + min) <= max; c++)
+		a[c] = c + min;
+	return (a);
 
 }
