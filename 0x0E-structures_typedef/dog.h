18b0360ad71c95e381427eeb5b5da90bdd9bbd86
Ro8s <rodrigo.delgado3132@gmail.com>
2021-03-08 18:48:49 +0000

    fix 0

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -1,5 +1,11 @@
 #ifndef A
 #define A
+/**
+ * struct dog - sturcttasdasdsadasdsadas
+ * @name: nameasdasdasdsad
+ * @age: ageasdasdasdasdsad
+ * @owner: ownerasdasdasdasdasdsada
+ **/
 struct dog
 {
 	char *name;
