43afe1756416d1ba9f8581faee5115efbff96015
Ro8s <rodrigo.delgado3132@gmail.com>
2021-03-10 14:29:30 +0000

    0 Finished

--- 0x0F-function_pointers/0-print_name.c
+++ 0x0F-function_pointers/0-print_name.c
@@ -3,14 +3,12 @@
 /**
  * print_name- print a name in uppercase
  * @name: name of the person
- *
+ * @f: ffff
  * Return: Nothing.
  */
 
 
 void print_name(char *name, void (*f)(char *))
 {
-	int a;
-	int b;
-	for (a = 0; name[a] != '\0'; a++)
-		
+	f(name);
+}
