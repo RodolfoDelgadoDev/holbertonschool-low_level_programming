f7fc1a328fe841e5a39d2be002cbf5d605f12944
Ro8s <rodrigo.delgado3132@gmail.com>
2021-03-23 18:51:17 +0000

    added ex 9

--- 0x13-more_singly_linked_lists/9-insert_nodeint.c
+++ 0x13-more_singly_linked_lists/9-insert_nodeint.c
@@ -10,9 +10,27 @@
 listint_t *insert_nodeint_at_index(listint_t **head, unsigned int idx, int n)
 {
 	listint_t *new;
-	unsigned int c;
+	listint_t *aux = *head;
+	unsigned int c = 0;
 
+	if (head == NULL)
+		return (NULL);
 	new = malloc(sizeof(listint_t));
 	if (new == NULL)
 		return (NULL);
+	if (idx == 0)
+	{
+		new->next = *head;
+		new = *head;
+		return (new);
+	}
+	while (c < idx - 1)
+	{
+		aux = aux->next;
+		c++;
+	}
 	new->n = n;
+	aux->next = new;
+	new = aux;
+	return (new);
+}
