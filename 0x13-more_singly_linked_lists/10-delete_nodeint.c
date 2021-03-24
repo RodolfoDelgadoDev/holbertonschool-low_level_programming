85548842a8a48fd31558212809c697ea8941779c
Ro8s <rodrigo.delgado3132@gmail.com>
2021-03-24 00:16:19 +0000

    Added 10

--- 0x13-more_singly_linked_lists/10-delete_nodeint.c
+++ 0x13-more_singly_linked_lists/10-delete_nodeint.c
@@ -1,1 +1,32 @@
-a
+#include "lists.h"
+
+/**
+ * delete_nodeint_at_index - delete a node
+ * @head: header
+ * @index: index
+ * Return: 1 or 0
+ */
+int delete_nodeint_at_index(listint_t **head, unsigned int index)
+{
+	listint_t *b = *head;
+	listint_t *deleted = *head;
+	unsigned int c;
+
+	if (index == 0)
+	{
+		*head = deleted->next;
+		free(deleted);
+		return (1);
+	}
+	while (c < index - 1 && b != NULL)
+	{
+		b = b->next;
+		c++;
+	}
+	if (index - 1 > c || b == NULL)
+		return (-1);
+        deleted = b->next;
+	b->next = deleted;
+	free(deleted);
+	return (1);
+}
