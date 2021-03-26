457363cb22cc3b63d6ed29dbdfd3f0974a0b5f2d
Ro8s <rodrigo.delgado3132@gmail.com>
2021-03-26 01:59:07 +0000

    fix betty

--- 0x14-bit_manipulation/2-get_bit.c
+++ 0x14-bit_manipulation/2-get_bit.c
@@ -1,8 +1,9 @@
 #include "holberton.h"
 /**
- * print_binary - print.
- * @n: number to print.
- * Return: Nothing.
+ * get_bit - get a bit.
+ * @n: number to binary.
+ * @index: index.
+ * Return: 0 if the number is 0, 1 in the another case and -1 if it's an error
  */
 int get_bit(unsigned long int n, unsigned int index)
 {
