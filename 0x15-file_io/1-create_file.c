67c2df04ebac8601d30c92b417dbadd9663c4558
Ro8s <rodrigo.delgado3132@gmail.com>
2021-03-29 23:35:08 +0000

    added ex 1

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,36 @@
+#include "holberton.h"
+
+/**
+ * create_file - creates a file.
+ * @filename: path
+ * @text_content: text_content
+ * Return: numbers
+ */
+int create_file(const char *filename, char *text_content)
+{
+	int fd, len = 0;
+	ssize_t wr;
+
+	if (filename == NULL)
+		return (-1);
+
+	fd = open(filename, O_CREAT | O_RDWR | O_TRUNC, 0600);
+
+	if (fd < 0)
+		return (-1);
+
+	if (text_content == NULL)
+		return (-1);
+
+	while (text_content[len] != '\0')
+		len++;
+
+	wr = write(fd, text_content, len);
+
+	if (wr < 0)
+		return (-1);
+
+	close(fd);
+
+	return (1);
+}
