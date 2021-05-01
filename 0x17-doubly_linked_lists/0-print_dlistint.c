4b82092fb50be4b0e6775823f55f8e47013c8f1c
Ro8s <rodrigo.delgado3132@gmail.com>
2021-05-01 17:10:32 +0000

    added ex 0, readme and .h

---
+++ 0x17-doubly_linked_lists/0-print_dlistint.c
@@ -0,0 +1,19 @@
+#include "lists.h"
+
+/**
+ * print_dlistint - Print d linked list.
+ * @h: header
+ * Return: number of nodes
+ */
+size_t print_dlistint(const dlistint_t *h)
+{
+	size_t c;
+
+	while (h != NULL)
+	{
+		printf("%d\n", h->n);
+		c++;
+		h = h->next;
+	}
+	return (c);
+}
