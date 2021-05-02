c64c3742457e87be97e2efd3efc5c2529f127188
Ro8s <rodrigo.delgado3132@gmail.com>
2021-05-02 17:56:20 +0000

    fix ex 5

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -6,7 +6,7 @@
  * @index: index
  * Return: the nth node
  */
-dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
 	unsigned int a = 0;
 
