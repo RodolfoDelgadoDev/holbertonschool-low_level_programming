4a1bacc942ef6f27f030f705484799c8fb4a6baf
Ro8s <rodrigo.delgado3132@gmail.com>
2021-05-02 17:55:16 +0000

    added ex 5 and mod lists.h

---
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -0,0 +1,21 @@
+#include "lists.h"
+
+/**
+ * get_dnodeint_at_index - nodes
+ * @head: header
+ * @index: index
+ * Return: the nth node
+ */
+dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index);
+{
+	unsigned int a = 0;
+
+	while (a < index)
+	{
+		if (head == NULL)
+			return (NULL);
+		head = head->next;
+		a++;
+	}
+	return (head);
+}
