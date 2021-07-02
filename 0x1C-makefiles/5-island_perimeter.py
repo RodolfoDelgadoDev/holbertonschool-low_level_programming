9cec4d36fe2d7e0d903dc90f9925c0baba945b56
Ro8s <rodrigo.delgado3132@gmail.com>
2021-07-02 23:52:28 +0000

    fix

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -1,5 +1,9 @@
 #!/usr/bin/python3
+''' Function to calculate the perimeter '''
+
+
 def island_perimeter(grid):
+    ''' Function '''
     perim = 0
     for i in range(len(grid)):
         for j in range(len(grid[i])):
