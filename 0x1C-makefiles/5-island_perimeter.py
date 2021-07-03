3f3c59c1a63be4b4bf6c989ae4f71b1e3b31d517
Ro8s <rodrigo.delgado3132@gmail.com>
2021-07-03 00:07:39 +0000

    fixed ex 5

--- 0x1C-makefiles/5-island_perimeter.py
+++ 0x1C-makefiles/5-island_perimeter.py
@@ -8,16 +8,24 @@ def island_perimeter(grid):
     for i in range(len(grid)):
         for j in range(len(grid[i])):
             if grid[i][j] == 1:
-                if i - 1 >= 0:
+                if i - 1 <= 0:
                     if grid[i - 1][j] == 0:
                         perim = perim + 1
+                else:
+                    perim = perim + 1
                 if i + 1 <= len(grid) - 1:
                     if grid[i + 1][j] == 0:
                         perim = perim + 1
+                else:
+                    perim = perim + 1
                 if j - 1 >= 0:
                     if grid[i][j - 1] == 0:
                         perim = perim + 1
+                else:
+                    perim = perim + 1
                 if j + 1 <= len(grid) - 1:
                     if grid[i][j + 1] == 0:
                         perim = perim + 1
+                else:
+                    perim = perim + 1
     return perim
