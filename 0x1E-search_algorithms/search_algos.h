a7e12b4234dd14ca5cbc264d1b3e6eb2ccd393e1
Ro8s <rodrigo.delgado3132@gmail.com>
2021-09-01 01:18:19 +0000

    updated header

--- 0x1E-search_algorithms/search_algos.h
+++ 0x1E-search_algorithms/search_algos.h
@@ -3,5 +3,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 int linear_search(int *array, size_t size, int value);
-
+int binary_search(int *array, size_t size, int value);
+void print_array(int *array, size_t first, size_t last);
 #endif
